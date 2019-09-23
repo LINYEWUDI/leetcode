5c63138380a0847fc56616ca683fe320f36b5edc
lyz <289373274@qq.com>
2019-09-23 09:57:48 +0800

    深信服题目完成

---
+++ src/day100.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+public class day100 {
+
+  /**
+   * 深信服题目
+   * 给出一个字符串数组,找出数组中第n长的字符串
+   */
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("请输入第几个：");
+    int i = scanner.nextInt();
+
+    List<String> list = new ArrayList<>();
+    list.add("abc");
+    list.add("ab");
+    list.add("a");
+    list.add("adfeeeffff");
+
+    Collections.sort(list, new Comparator<String>() {
+      @Override
+      public int compare(String str1, String str2) {
+        if (str1 == null || "".equals(str1)) {
+          return 1;
+        }
+        if (str2 == null || "".equals(str2)) {
+          return 1;
+        }
+
+        int len1 = str1.length();
+        int len2 = str2.length();
+        return len2 - len1;
+      }
+    });
+
+
+    for (int j = 0; j < list.size(); j++) {
+      System.out.println(list.get(j));
+    }
+    System.out.println("第"+i+"个是"+list.get(i));
+
+
+  }
+
+
+}
--- src/day3.java
+++ src/day3.java
@@ -1,6 +1,7 @@
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class day3 {
 
@@ -88,14 +89,37 @@ public class day3 {
     }
 
 
+    /**
+     * 官方解法2
+     */
+
+    public static int lengthOfLongestSubstring3(String s) {
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            // try to extend the range [i, j]
+            if (!set.contains(s.charAt(j))){
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            }
+            else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return ans;
+    }
+
+
+
     public static void main(String[] args) {
-        String s = "abcabcbb";
+        String s = "abcaefbb";
         String s1 = "pwwkew";
         String s2 = "cdd";
         String s3 = " ";
         String s4 = "aab";
 
 
-        System.out.println(lengthOfLongestSubstring1(s));
+        System.out.println(lengthOfLongestSubstring3(s));
     }
 }
