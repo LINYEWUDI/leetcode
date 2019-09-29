baff618ca241e8eb2dfe6093316700d3b3a865fb
lyz <289373274@qq.com>
2019-09-29 09:22:12 +0800

    12题贪心思考,14题最长公共前缀注释

--- src/day12.java
+++ src/day12.java
@@ -1,13 +1,37 @@
-import org.omg.PortableInterceptor.INACTIVE;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-
 public class day12 {
-
+  /**
+   *例如， 罗马数字 2 写做 II ，即为两个并列的 1。12 写做 XII ，即为 X + II 。 27 写做  XXVII, 即为 XX + V + II 。
+   *
+   * 通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，例如 4 不写做 IIII，而是 IV。数字 1 在数字 5 的左边，所表示的数等于大数 5 减小数 1 得到的数值 4 。同样地，数字 9 表示为 IX。这个特殊的规则只适用于以下六种情况：
+   *
+   * I 可以放在 V (5) 和 X (10) 的左边，来表示 4 和 9。
+   * X 可以放在 L (50) 和 C (100) 的左边，来表示 40 和 90。 
+   * C 可以放在 D (500) 和 M (1000) 的左边，来表示 400 和 900。
+   * 给定一个整数，将其转为罗马数字。输入确保在 1 到 3999 的范围内。
+   *
+   * 输入: 3
+   * 输出: "III"
+   *
+   * 输入: 4
+   * 输出: "IV"
+   *
+   * 输入: 9
+   * 输出: "IX"
+   *
+   * 输入: 58
+   * 输出: "LVIII"
+   * 解释: L = 50, V = 5, III = 3.
+   *
+   * 输入: 1994
+   * 输出: "MCMXCIV"
+   * 解释: M = 1000, CM = 900, XC = 90, IV = 4.
+   */
   public static String intToRoman(int num) {
-    //分别初始化两个数组
+    /**
+     * 思想：贪心算法.初始化两个数组后,两个数组中的每一个元素是对应的。
+     * 同时传进来数字依次从大到小去匹配数组中的元素,碰到第一个比传进来数字小的后，
+     * 则减去这个匹配上的元素，同时
+     */
     int[] nums = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
     String[] romans = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
     StringBuffer stringBuffer = new StringBuffer();
--- src/day14.java
+++ src/day14.java
@@ -1,35 +1,49 @@
 import java.util.ArrayList;
 import java.util.Collections;
 
+/**
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ * 如果不存在公共前缀，返回空字符串 ""。
+ * 示例 1:
+ * 输入: ["flower","flow","flight"]
+ * 输出: "fl"
+ *
+ * 示例 2:
+ * 输入: ["dog","racecar","car"]
+ * 输出: ""
+ * 解释: 输入不存在公共前缀。
+ */
 public class day14 {
 
   public static String longestCommonPrefix(String[] strs) {
-
+    //数组长度为0时,则直接返回空串
     if (strs.length == 0) return "";
     ArrayList<Character> list = new ArrayList<>();
 
     int flag = 0;
+    //获取字符串数组中最短长度
     int minLen = strs[0].length();
     for (int i = 1; i < strs.length; i++) {
       if (minLen > strs[i].length()){
         minLen = strs[i].length();
       }
     }
-
+    //外层循环控制数组中最短字符串的长度,顺次遍历每一个字母
     for (int i = 0; i < minLen; i++) {
       char c = strs[0].charAt(i);
+      //内层控制数组中每一个字符串
       for (int j = 0; j < strs.length; j++) {
-
+        //判断数组中每一个字符串的每一个字母是否相同,相同则将标记变量置为1
         if (strs[j].charAt(i) != c){
           flag = 1;
         }
       }
-
+      //相同的字母放入集合中
       if (flag == 0){
         list.add(c);
       }
     }
-
+    //拼接成字符串后返回
     StringBuffer stringBuffer = new StringBuffer();
     for (int i = 0; i < list.size(); i++) {
       System.out.println(list.get(i));
