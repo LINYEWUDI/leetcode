48ca5b60380ee15fdb34dc2b0b568aff38a9a27e
LYZ <289373274@qq.com>
2019-10-13 10:52:43 +0800

    完善17题思考

--- src/day1.java
+++ src/day1.java
@@ -1,5 +1,3 @@
-import org.junit.Test;
-
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
--- src/day17.java
+++ src/day17.java
@@ -1,48 +1,43 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class day17 {
 
+  /**
+   *  思想：
+   *  1.初始化九宫格数字对应的字母在数组中
+   *  2.依次遍历输入的数字，将第一个数字对应的字母放入队列中
+   *  当遍历到第二个数字时，出队列的都是每一个数字对应的字母  再和第二个数字对应的字母匹配后入队列
+   *  3.遍历到第三个数字时，出队列的都是第一个数字和第二个数字匹配上的字母，再和第三个字母匹配
+   *  4.按此规律向下走
+   */
   public static List<String> letterCombinations(String digits) {
-    HashMap<String, String> hashMap = new HashMap<>();
-    hashMap.put("2","abc");
-    hashMap.put("3","def");
-    hashMap.put("4","ghi");
-    hashMap.put("5","jkl");
-    hashMap.put("6","mno");
-    hashMap.put("7","pqrs");
-    hashMap.put("8","tuv");
-    hashMap.put("9","wxyz");
-
-    if ("".equals(digits)){
-      return new ArrayList<>();
-    }
-
+    //初始化队列
     LinkedList<String> list = new LinkedList<>();
 
+    //判断字符串为空的情况
+    if (digits.isEmpty()) return list;
+
+    list.add("");
+
+    //初始化九宫格
+    String[] mapping = new String[] {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
     for (int i = 0; i < digits.length(); i++) {
-      char c = digits.charAt(i);
-      list.add(hashMap.get(String.valueOf(c)));
-    }
-
-    ArrayList<String> strings = new ArrayList<>();
-
-    for (int i = 0; i < list.get(0).length(); i++) {
-      for (int j = 0; j < list.get(1).length(); j++) {
-        StringBuffer stringBuffer = new StringBuffer();
-        stringBuffer.append(list.get(0).charAt(i));
-        stringBuffer.append(list.get(1).charAt(j));
-        String s = stringBuffer.toString();
-        strings.add(s);
+      //初始化输入的数字
+      Integer integer = new Integer(String.valueOf(digits.charAt(i)));
+      while (list.peek().length() == i){
+        String remove = list.remove();
+        for (char c : mapping[integer].toCharArray()){
+          list.add(remove+c);
+        }
       }
     }
-
-    return strings;
+    return list;
   }
 
+
   public static void main(String[] args) {
-    System.out.println(letterCombinations("23"));
+    System.out.println(letterCombinations("236"));
+    System.out.println(letterCombinations("236").size());
   }
 }
